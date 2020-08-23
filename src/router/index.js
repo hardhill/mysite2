9d4f5e25f21c064fceeb721cfe8ea5155562c6bd
hardhill <hardhill@mail.ru>
2020-08-23 22:10:54 +0800

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Project01 from '../views/Project01.vue'
+import NotFound from '../views/404.vue'
 
 Vue.use(VueRouter)
 
@@ -11,13 +13,15 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/canvas/p1',
+    name: 'Project01',
+    component: Project01
+
+  },
+    {
+      path:'*',
+      component: NotFound
+    }
 ]
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,75 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="main-block">
+      <div class="header">ПРОЕКТЫ</div>
+      <div class="element0" @click="GoProject(0)">Пример использования CANVAS. Игра на `чистом` JS.
+        Сделана по материалам сайта <a href="https://developer.mozilla.org/ru/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript">MDN webdocs</a>
+      </div>
+      <div class="element0" @click="GoProject(1)">Эксперементы с фреймворком <span class="flash">Matter JS</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
+  },
+  methods:{
+    GoProject:function(n){
+      switch (n) {
+        case 0:{
+          this.$router.push({name:'Project01'})
+          break
+        }
+        case 1:{
+          break
+        }
+      }
+    }
   }
 }
 </script>
+<style scoped>
+  .container {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background-image: url(/images/Tower.jpg) ;
+    background-size: cover;
+
+  }
+  .main-block{
+    position: absolute;
+    top: 3rem;
+    left: 2rem;
+    width: 30rem;
+    background-color: rgba(53, 76, 85, 0.4);
+    color:#F5F5F5;
+  }
+  .header{
+    text-align: center;
+    padding: 1rem 1rem;
+    width: 100%;
+    color: aliceblue;
+  }
+  .flash{
+    color: #ff5263;
+  }
+  .element0{
+    padding: 0.5rem 2rem;
+    font-size: 1.2rem;
+    cursor: pointer;
+
+  }
+  .element0:hover{
+    background-color: rgba(60, 86, 95, 0.4);
+  }
+</style>
